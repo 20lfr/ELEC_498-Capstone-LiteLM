5f3dbb0f5a1e85efc0fd14fbe14762d207cc0dc9
Luka <lukarod72@gmail.com>
2026-01-05 17:16:49 -0500

    Saving before making large changes

--- vitis_simulations/Top_Module/_ide/workspace_journal.py
+++ vitis_simulations/Top_Module/_ide/workspace_journal.py
@@ -1,6 +1,11 @@
-# 2025-12-17T17:21:39.102080
+# 2026-01-05T17:14:16.479192
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="Top_Module")
+
+comp = client.get_component(name="FSM_and_Control_FSM_top_module")
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="SYNTHESIS")
 
